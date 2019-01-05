1cfedebffef2c91194412e6e5ec786f2f52e6af8
tranebest <tranebest@mail.ru>
2019-01-05 11:28:14 -0500

    Make a basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_225157) do
+ActiveRecord::Schema.define(version: 2019_01_05_160906) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_01_02_225157) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
