6de6ba53ad12ebf9d4479ebef8f2072a33d497ed
tranebest <tranebest@mail.ru>
2019-01-24 07:16:01 -0500

    Deleted some files

--- db/migrate/20190124121030_create_microposts.rb
+++ db/migrate/20190124121030_create_microposts.rb
@@ -2,9 +2,10 @@ class CreateMicroposts < ActiveRecord::Migration[5.2]
   def change
     create_table :microposts do |t|
       t.text :content
-      t.integer :user_id
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
+    
   end
 end
